5bc9e6f4b98687992d81a4cfa3303dabbabc97c3
ciechanowski <mciechanowski1991@gmail>
2025-03-01 22:08:04 +0100

    Wprowadzenie i zmienne: Zmienne

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        "Cześć!" 100 106.78 'g' true false
+        //Jednoczesna deklaracja i inicjalizacja
+        String cake = "jabłecznik";
+        //Osobna deklaracja
+        //String cake;
+        //i inicjalizacja (przypisanie wartości)
+        //cake = "jabłecznik";
+
+        System.out.println("Bardzo lubię " + cake);
+        System.out.println("Jedyny dobry " + cake + " to ten bez rodzynek");
+        System.out.println("Na " + cake + " jest osobny żołądek!");
     }
 }
