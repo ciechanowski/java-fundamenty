4a67123f076e88ccdf2d6b1076327ca3cf6153b0
ciechanowski <mciechanowski1991@gmail>
2025-03-01 22:24:39 +0100

    Wprowadzenie i zmienne: Null

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        var cake = "jabłecznik";
-        //Ale tak nie zadziała:
-        //var cake;
-        //cake = "jabłecznik";
+        String nothing = null;
+        String empty = "";
 
-        System.out.println("Bardzo lubię " + cake);
-        System.out.println("Jedyny dobry " + cake + " to ten bez rodzynek");
-        System.out.println("Na " + cake + " jest osobny żołądek!");
+        System.out.println(empty.length());
+        System.out.println(nothing.length());
     }
 }
