79496cb501f2f930d2febd2c5779d8e1836e2b3a
ciechanowski <mciechanowski1991@gmail>
2025-03-01 22:45:21 +0100

    Wprowadzenie i zmienne: Wiersz polecenia

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(hi);
-        sayHi();
-        test = "asd";
+        System.out.println("Siemanko!");
     }
 }
+
+// otwórz wiersz poleceń z miejsca, gdzie znajduje się plik Main.java
+// wpisz javac Main.java
