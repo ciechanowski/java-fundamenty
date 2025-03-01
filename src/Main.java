8fe4b447dffd35778b98d267995956d83d779343
ciechanowski <mciechanowski1991@gmail>
2025-03-01 22:36:10 +0100

    Wprowadzenie i zmienne: Błędy

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String nothing = null;
-        String empty = "";
-
-        System.out.println(empty.length());
-        System.out.println(nothing.length());
+        System.out.println(hi);
+        sayHi();
+        test = "asd";
     }
 }
