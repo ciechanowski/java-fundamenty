13ef9c261040c7fa888e48fcb32d8a7d224a9f4c
ciechanowski <mciechanowski1991@gmail>
2025-03-01 16:34:51 +0100

    Wprowadzenie i zmienne: Tekst w konsoli

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Nowa linia");
+        System.out.println("To też jest w nowej linii");
+
+        System.out.print("Bez nowej ");
+        System.out.print("linii");
     }
 }
